fc0f30832aaa4bd8556744b2ce2648204031ac9a
ido30902 <idobarabi@gmail.com>
2022-02-26 16:31:57 +0200

    modified:   src/App.java 	modified:   src/FrameManager.java

--- src/App.java
+++ src/App.java
@@ -5,7 +5,7 @@ public class App {
         String startingFen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR";
 
         FrameManager frameManager = new FrameManager();
-        Board board = new Board("rnbqk2r/pp1pppbp/2p2np1/8/3P4/2N1P3/PPP2PPP/R1BQKBNR");
+        Board board = new Board(startingFen);
         
         board.printBoard();
 
--- src/FrameManager.java
+++ src/FrameManager.java
@@ -1,16 +1,34 @@
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class FrameManager 
 {
     
     private JFrame frame;
+    private JButton[][] chessBoardSquares;
+    private JPanel surface;
 
     public FrameManager() 
+    {
+        initFrame();
+        
+    }
+
+
+    private void initFrame()
     {
         frame = new JFrame("Chess");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(800, 800);
         frame.setVisible(true);
+    }
+    
+
+    private void initBoard()
+    {
+        chessBoardSquares = new JButton[8][8];
+
     }
 
     
